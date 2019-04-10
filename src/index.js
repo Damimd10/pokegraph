cc52f12acb3cd48868cfe146884d07478f041f4e
Damian <damianmdst@gmail.com>
2019-04-09 23:47:02 -0300

    Delete typeDefs from index

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ const { GraphQLServer } = require('graphql-yoga');
 const { fileLoader, mergeTypes } = require('merge-graphql-schemas');
 const path = require('path');
 
-const typeDefs = `
-  type Query {
-    berries(id: String!): Berry
-  }
-  type Berry {
-    id: Int!
-    name: String!
-  }
-`;
-
 const resolversArray = fileLoader(path.join(__dirname, './**/resolvers.*'));
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'));
 const typesMerged = mergeTypes(typesArray);
