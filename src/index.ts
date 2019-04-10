0a9fa8021b43840009f1b832feb4d1d264859c8a
Damian <damianmdst@gmail.com>
2019-04-10 14:52:03 -0300

    Add prettier config as style guide of code, and eslint with ts recommended plugin for code lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import { GraphQLServer } from 'graphql-yoga';
-import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+import {GraphQLServer} from 'graphql-yoga';
+import {fileLoader, mergeTypes} from 'merge-graphql-schemas';
 import * as path from 'path';
 
 const resolversArray = fileLoader(path.join(__dirname, './**/resolvers.*'));
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'));
 const typesMerged = mergeTypes(typesArray);
 
-const options = { port: 4000 };
-const server = new GraphQLServer({ typeDefs: typesMerged, resolvers: resolversArray });
-server.start(options, () => console.log('Server is running on localhost:' + options.port));
+const options = {port: 4000};
+const server = new GraphQLServer({
+  typeDefs: typesMerged,
+  resolvers: resolversArray,
+});
+
+server.start(options, () =>
+  console.log('Server is running on localhost:' + options.port)
+);
