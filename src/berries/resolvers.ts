c03f8e4741e86b91428cd0faf5ae6cbd664c80e1
Damian <damianmdst@gmail.com>
2019-04-16 13:36:31 -0300

    Refactor with new Service class berries endpoint

--- src/berries/resolvers.ts
+++ src/berries/resolvers.ts
@@ -1,8 +1,19 @@
-import { getAll, getBerryById } from './controller';
+import Service from '../shared/service/Service';
+import { IExtraParams, IPaginationParams } from '../shared/service/api';
+import IAPIResourceList from '../models/IAPIResourceList';
+import IBerry from '../models/IBerry';
+
+const endpoint = 'berry';
+
+const service = new Service(endpoint);
 
 export default {
   Query: {
-    getAllBerries: (_, { offset = 0, limit = 20 }) => getAll(offset, limit),
-    getBerry: (_, { id }) => getBerryById(id),
+    getAllBerries: (_, { offset = 0, limit = 20 }) =>
+      service.getAllResources<IPaginationParams, IAPIResourceList, IBerry[]>(
+        offset,
+        limit
+      ),
+    getBerry: (_, { id }) => service.getResourceById<IExtraParams, IBerry>(id),
   },
 };
