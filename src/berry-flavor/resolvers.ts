08db6ba5972373979f3360e265307458362767a8
Damian <damianmdst@gmail.com>
2019-04-16 13:51:02 -0300

    Refactor with new service class berry-flavor endpoint

--- src/berry-flavor/resolvers.ts
+++ src/berry-flavor/resolvers.ts
@@ -1,8 +1,20 @@
-import { getAll, getBerryFlavorById } from './controller';
+import Service from '../shared/service/Service';
+import { IExtraParams, IPaginationParams } from '../shared/service/api';
+import IBerryFlavor from '../models/IBerryFlavor';
+import IAPIResourceList from '../models/IAPIResourceList';
+
+const endpoint = 'berry-flavor';
+const service = new Service(endpoint);
 
 export default {
   Query: {
-    getAllBerryFlavor: (_, { offset = 0, limit = 20 }) => getAll(offset, limit),
-    getBerryFlavor: (_, { id }) => getBerryFlavorById(id),
+    getAllBerryFlavor: (_, { offset = 0, limit = 20 }) =>
+      service.getAllResources<
+        IPaginationParams,
+        IAPIResourceList,
+        IBerryFlavor[]
+      >(offset, limit),
+    getBerryFlavor: (_, { id }) =>
+      service.getResourceById<IExtraParams, IBerryFlavor>(id),
   },
 };
