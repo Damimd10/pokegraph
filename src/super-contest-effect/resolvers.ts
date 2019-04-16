8bfb1db5ac4cef93bd978e911c997eade3d21e65
Damian <damianmdst@gmail.com>
2019-04-16 14:56:59 -0300

    Refactor super-contest-effect with new service class

--- src/super-contest-effect/resolvers.ts
+++ src/super-contest-effect/resolvers.ts
@@ -1,9 +1,21 @@
-import { getAll, getSuperContestEffectById } from './controller';
+import Service from '../shared/service/Service';
+import { IExtraParams, IPaginationParams } from '../shared/service/api';
+import ISuperContestEffect from '../models/ISuperContestEffect';
+import IAPIResourceList from '../models/IAPIResourceList';
+
+const endpoint = 'super-contest-effect';
+
+const service = new Service(endpoint);
 
 export default {
   Query: {
     getAllSuperContestEffect: (_, { offset = 0, limit = 20 }) =>
-      getAll(offset, limit),
-    getSuperContestEffect: (_, { id }) => getSuperContestEffectById(id),
+      service.getAllResources<
+        IPaginationParams,
+        IAPIResourceList,
+        ISuperContestEffect[]
+      >(offset, limit),
+    getSuperContestEffect: (_, { id }) =>
+      service.getResourceById<IExtraParams, ISuperContestEffect>(id),
   },
 };
