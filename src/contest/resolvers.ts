e3d8684d78376881dd4caedd68389037fa45edfb
Damian <damianmdst@gmail.com>
2019-04-16 14:51:49 -0300

    Refactor with service class contest endpoint

--- src/contest/resolvers.ts
+++ src/contest/resolvers.ts
@@ -1,8 +1,20 @@
-import { getAll, getContestById } from './controller';
+import Service from '../shared/service/Service';
+import { IExtraParams, IPaginationParams } from '../shared/service/api';
+import IContest from '../models/IBerryFlavor';
+import IAPIResourceList from '../models/IAPIResourceList';
+
+const endpoint = 'contest-type';
+
+const service = new Service(endpoint);
 
 export default {
   Query: {
-    getAllContest: (_, { offset = 0, limit = 20 }) => getAll(offset, limit),
-    getContest: (_, { id }) => getContestById(id),
+    getAllContest: (_, { offset = 0, limit = 20 }) =>
+      service.getAllResources<IPaginationParams, IAPIResourceList, IContest[]>(
+        offset,
+        limit
+      ),
+    getContest: (_, { id }) =>
+      service.getResourceById<IExtraParams, IContest>(id),
   },
 };
