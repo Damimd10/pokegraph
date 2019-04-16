379db03cfd94a189a3c2db4e0500bfa25895b7d6
Damian <damianmdst@gmail.com>
2019-04-16 14:55:12 -0300

    Refactor contest endpoint with the new service class

--- src/contest-effect/resolvers.ts
+++ src/contest-effect/resolvers.ts
@@ -1,9 +1,21 @@
-import { getAll, getContestEffectById } from './controller';
+import Service from '../shared/service/Service';
+import { IExtraParams, IPaginationParams } from '../shared/service/api';
+import IContestEffect from '../models/IContestEffect';
+import IAPIResourceList from '../models/IAPIResourceList';
+
+const endpoint = 'contest-effect';
+
+const service = new Service(endpoint);
 
 export default {
   Query: {
     getAllContestEffects: (_, { offset = 0, limit = 20 }) =>
-      getAll(offset, limit),
-    getContestEffects: (_, { id }) => getContestEffectById(id),
+      service.getAllResources<
+        IPaginationParams,
+        IAPIResourceList,
+        IContestEffect[]
+      >(offset, limit),
+    getContestEffects: (_, { id }) =>
+      service.getResourceById<IExtraParams, IContestEffect>(id),
   },
 };
