96b655de01460659f3cef5efe12457b57eaaab70
Damian <damianmdst@gmail.com>
2019-04-20 20:19:19 -0300

    Add berry flavor sub service for berry endpoint

--- src/berries/resolvers.ts
+++ src/berries/resolvers.ts
@@ -1,11 +1,28 @@
+import { assoc, path, prop } from 'ramda';
+
 import Service from '../shared/service/Service';
 import { IExtraParams, IPaginationParams } from '../shared/service/api';
+
 import IAPIResourceList from '../models/IAPIResourceList';
 import IBerry from '../models/IBerry';
+import IBerryFlavor from '../models/IBerryFlavor';
 
 const endpoint = 'berry';
+const flavorEndpoint = 'berry-flavor';
 
 const service = new Service(endpoint);
+const flavorService = new Service(flavorEndpoint);
+
+const mappedFlavors = flavors =>
+  flavors.map(async x => {
+    const currentFlavor = path(['flavor', 'name'], x);
+    const flavorResult = await flavorService.getResourceById<
+      IExtraParams,
+      IBerryFlavor
+    >(currentFlavor);
+
+    return assoc('flavor', flavorResult, x);
+  });
 
 export default {
   Query: {
@@ -14,6 +31,11 @@ export default {
         offset,
         limit
       ),
-    getBerry: (_, { id }) => service.getResourceById<IExtraParams, IBerry>(id),
+    getBerry: async (_, { id }) => {
+      const result = await service.getResourceById<IExtraParams, IBerry>(id);
+      const flavors = prop('flavors', result);
+
+      return assoc('flavors', mappedFlavors(flavors), result);
+    },
   },
 };
